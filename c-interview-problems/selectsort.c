94ab9c74ead055b9b7683b3f42cf00bfde2c565c
zyprex <aintzy@foxmail.com>
2020-06-08 11:48:23 +0800

    format file 'selectsort.c'

--- c-interview-problems/selectsort.c
+++ c-interview-problems/selectsort.c
@@ -1,30 +1,27 @@
+/*
+ * select sort
+ */
 #include <stdio.h>
 #define ARRL(X) sizeof(X)/sizeof(*X)
-/*
-   [0...                            len-2]
-        [1...                len-1]
-            [2...            len-1]
-                [3...        len-1]
-                    [len-2...len-1]
-*/
-void selectsort(int s[], int len)
+
+void selectsort(int arr[], int len)
 {
     int i, j, k;
     for( i = 0; i < len-1; i += 1 ) {
         k = i;
         for( j = i+1; j < len; j += 1 ) {
-            if( s[k] > s[j] ) {
+            if( arr[k] > arr[j] ) {
                 k = j;
             }
         }
         if( k != i ) {
-            int tmp = s[k];
-            s[k] = s[i];
-            s[i] = tmp;
+            int tmp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = tmp;
         }
     }
     for( i = 0; i < len; i += 1 ) {
-        printf( "%d ", s[i] );
+        printf( i == len-1 ? "%d" : "%d ", arr[i] );
     }
 }
 
