b281e56c7957c6f9a0404bb7dd5f6ea63a1d2958
zyprex <aintzy@foxmail.com>
2020-06-21 19:07:41 +0800

    M: + a better bubblesort use flag

--- c-interview-problems/bubblesort.c
+++ c-interview-problems/bubblesort.c
@@ -17,12 +17,29 @@ void bubblesort(int arr[], int len)
     }
 }
 
+void bubblesort2(int arr[], int len)
+{
+    int i, j, flag;
+    for( i = 0; i < len - 1; i += 1 ) {
+        flag = 0;
+        for( j = 0; j < len - 1 - i; j += 1 ) {
+            if( arr[j] > arr[j+1] ) {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                flag = 1;
+            }
+        }
+        if( !flag )
+            break;
+    }
+}
 
 int main(int argc, const char *argv[])
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int len = sizeof(arr)/sizeof(arr[0]);
-    bubblesort(arr, len);
+    bubblesort2(arr, len);
     int i;
     for( i = 0; i < len; i += 1 ) {
         printf( "%d ", arr[i] );
